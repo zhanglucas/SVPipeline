62f1c4a94b3e7324ba5231bd095ca70afcb236c9
lukezhan <zhanglucasjifeng@gmail.com>
2019-09-11 16:31:05 -0700

    finished cnmops

--- cn.mops/cnmops.R
+++ cn.mops/cnmops.R
@@ -5,8 +5,23 @@ Pathtobam = "/u/home/l/lukezhan/data_master/original_data/LP/"
 setwd(Pathtobam)
 BAMFiles <- list.files(path=Pathtobam,pattern=".bam$")
 bamDataRanges <- getReadCountsFromBAM(BAMFiles)
-res <- cn.mops(bamDataRanges)
+trim(bamDataRanges)
+(bamDataRanges)
+data(cn.mops)
+ls()
+head(XRanges[,1:3])
+resCNMOPS <- cn.mops(XRanges)
+resCNMOPS <- calcIntegerCopyNumbers(resCNMOPS)
+(resCNMOPS)
+cnvs(resCNMOPS)[1:5]
+
+##exporting results
+library(cn.mops); data(cn.mops)
 result <- calcIntegerCopyNumbers(cn.mops(XRanges))
 segm <- as.data.frame(segmentation(result))
 CNVs <- as.data.frame(cnvs(result))
 CNVRegions <- as.data.frame(cnvr(result))
+outdir="/u/scratch/l/lukezhan/cnmopsout/result.csv"
+write.csv(segm,file=outdir)
+write.csv(CNVs,file=outdir)
+write.csv(CNVRegions,file=outdir)
